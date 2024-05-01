9bfc7b8899edf171470214d2d1e9fe29f6868093
Jack <144901250+mcnlty@users.noreply.github.com>
2024-05-01 10:21:13 +0000

    Update flake.nix

--- default/flake.nix
+++ default/flake.nix
@@ -3,19 +3,20 @@
 
   inputs = {
     # NixPkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-#    home-manager.url = "github:nix-community/home-manager";
-#    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    #home-manager.url = "github:nix-community/home-manager/release-23.11";
+    #home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: {
     nixosConfigurations = {
-    # Replace "Default" with your hostname
-    default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [./configuration.nix];
+      # Replace "Default" with your hostname #
+      default = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [./configuration.nix];
       };
     };
   };

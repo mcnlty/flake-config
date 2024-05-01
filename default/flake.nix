4b06cb5d684dbe519d42e03301b14f5b11ad6e36
Jack <144901250+mcnlty@users.noreply.github.com>
2024-05-01 05:12:45 +0000

    Update flake.nix

--- default/flake.nix
+++ default/flake.nix
@@ -6,8 +6,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+#    home-manager.url = "github:nix-community/home-manager";
+#    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {

925663f64025dca06365eafee0cca278cfd950af
Jack <144901250+mcnlty@users.noreply.github.com>
2024-05-01 04:52:13 +0000

    Create flake.nix

---
+++ default/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "NixOS Config Flake";
+
+  inputs = {
+    # NixPkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+    # Replace "Default" with your hostname
+    default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [./configuration.nix];
+      };
+    };
+  };
+}
